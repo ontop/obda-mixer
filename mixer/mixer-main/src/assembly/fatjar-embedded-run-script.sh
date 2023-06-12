88b5e72f43e527b37f6b80a1a5e0982f9ca435cc
Francesco Corcoglioniti <francesco.corcoglioniti@gmail.com>
2023-06-12 16:42:56 +0200

    Updated Maven setup, removed mixer-distribution, using shade to generate runnable fatjar (in mixer-main)

---
+++ mixer/mixer-main/src/assembly/fatjar-embedded-run-script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -n "$JAVA_HOME" ] && [ -x "$JAVA_HOME/bin/java" ]; then
+    JAVA="$JAVA_HOME/bin/java"
+else
+    JAVA=java
+fi
+
+declare -a opts
+opts=()
+for o in "$@"; do
+    if [[ "$o" =~ ^"--jars=".*$ ]]; then
+        jars="${jars+$jars:}${o#*=}"
+    else
+        opts+=("$o")
+    fi
+done
+
+# shellcheck disable=SC2086
+exec $JAVA $JAVA_OPTS \
+    -cp "$0${jars+:$jars}${JAVA_CLASSPATH+:$JAVA_CLASSPATH}" \
+    it.unibz.inf.mixer_main.execution.MixerMain \
+    "${opts[@]}"
